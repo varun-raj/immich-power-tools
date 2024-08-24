c7212cc53b093df2d79de3bd42e299d82d3df272
Varun Raj <mailme@varunraj.in>
2024-08-24 08:14:53 +0530

    fix: disabled nextjs image optimization due to sharp dependencies
    Signed-off-by: Varun Raj <mailme@varunraj.in>

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -21,7 +21,7 @@ export default function Sidebar() {
       <div className="flex h-full max-h-screen flex-col gap-2">
         <div className="flex h-14 items-center justify-between border-b px-4 lg:h-[60px] lg:px-6">
           <Link href="/" className="flex items-center  gap-2 font-semibold">
-            <Image
+            <img
               src="/favicon.png"
               width={32}
               height={32}
