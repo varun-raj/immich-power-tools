4dd64aa1ff47d1464f325c0344c855a37fb5ed9d
Varun Raj <varun@skcript.com>
2024-11-30 14:23:27 +0530

    fix: tailwind fixes
    Signed-off-by: Varun Raj <varun@skcript.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ const config = {
   		mono: [ ...fontFamily.mono ]
   	},
   	container: {
-  		center: 'true',
+  		center: true,
   		padding: '2rem',
   		screens: {
   			'2xl': '1400px'
@@ -78,27 +78,9 @@ const config = {
   				to: {
   					height: '0'
   				}
-  			},
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
   			}
   		},
   		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out',
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
